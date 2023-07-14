83761effe15e191e8c8b4d8abef8a3f3742070a9
Ketlyn <ketlyn@d18.com.br>
2023-07-14 11:22:37 -0300

    Tentativa falida.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
-     :heroes
+    resources :heroes, only: [:create]
   end
+  get '/api/heroes/:id', to: 'api/heroes#show', as: 'api_hero_url'
 end
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
