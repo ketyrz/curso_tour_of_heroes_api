49b14df8bfeaf18b9c7bf7aa2d906ec6b9cb8ff1
Ketlyn <ketlyn@d18.com.br>
2023-07-14 09:48:13 -0300

    Reosurces.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
-    resources :heroes
+     :heroes
   end
 end
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
